18dd64d9ff4b3cd6cf374453f1bad5d9b1a801a4
TBUGOP <umanggarg2002@gmail.com>
2023-02-22 03:30:38 +0530

    commit

---
+++ mapping.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract demo 
+{
+    mapping(uint=>string) public roll_no;
+
+    function setter (uint keys, string memory value) public
+    {
+        roll_no[keys]=value;
+    }
+
+}
