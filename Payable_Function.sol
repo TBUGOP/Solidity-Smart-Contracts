0051d3565626306c63e5d2bc4c93e371399c272d
TBUGOP <umanggarg2002@gmail.com>
2023-02-22 23:52:44 +0530

    commit

---
+++ Payable_Function.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.3 <0.9.0;
+
+contract pay
+{
+    address payable user = payable(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+
+    function payEther() public payable
+    {
+
+    }
+
+    function getBalance() public view returns(uint)
+    {
+        return address(this).balance;
+    }
+
+    function sendEtherAccount() public 
+    {
+        user.transfer(1 ether);
+    }
+}
