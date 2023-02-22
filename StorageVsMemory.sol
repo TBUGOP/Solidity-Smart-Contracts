430964968ffd7b0cf262129e684fd31749ef6453
TBUGOP <umanggarg2002@gmail.com>
2023-02-22 22:05:16 +0530

    commit

---
+++ StorageVsMemory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.3 <0.9.0;
+
+contract demo 
+{
+    string[] public student = ['Ravi', 'Rita', 'Aman'];
+
+    function mem() public view
+    {
+        string[] memory s1=student;
+        s1[0]='Akash';
+    }
+
+    function sto() public 
+    {
+        string[] storage s1=student;
+        s1[0]='Akash';
+    }
+}
--- mapping.sol
+++ mapping.sol
@@ -4,11 +4,23 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract demo 
 {
-    mapping(uint=>string) public roll_no;
 
+    struct Student {
+        string name;
+        uint class;
+    }
+
+    /*
+    mapping(uint=>string) public roll_no;
     function setter (uint keys, string memory value) public
     {
         roll_no[keys]=value;
     }
+    */
 
+    mapping(uint=>Student) public data;
+    function setter (uint _roll, string memory _name, uint _class) public
+    {
+        data[_roll]=Student(_name, _class);
+    }
 }
